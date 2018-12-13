20ba8d6c46ef82926407717c61d3c6adb4d4107a
VasilchenkoSergei <chainsaw86@mail.ru>
2018-12-13 23:59:25 +0300

    account-list to do, mobile, breakpoints

--- src/js/script.js
+++ src/js/script.js
@@ -1,163 +1,64 @@
+
 window.onload = function() {
-  document.querySelector('.page-header__title').style.left = '0px';
-  document.querySelector('.page-header__descr').style.left = '0px';
-  // document.querySelector('.page-header').style.backgroundPos = '0px';
+
+  const loadData = function(data) {
+    const list = getElementById('account-list__items')
+    
+    data.forEach(item => {
+      const card =
+        `<article class="appartments-item" tabindex="0">
+
+        </article>`
+      list.appendChild(card)
+    })
+  }
+
+  $.ajax('https://api.myjson.com/bins/6dbie!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+    .done(data => {
+    loadData(data)
+  })
+    .fail(function() {
+    alert('error');
+  })
+
 };
 
 
-$(document).ready(function(){
 
-$('.popup').hide();
+document.querySelector('.time-block__btn').onclick = function() {
 
-$('.promo__burger').on('click', function() {
-  $('.main-nav').toggleClass('.active');
-  $('.main-nav').toggle();
-});
+  const loadData = function(data) {
+    const list = getElementById('account-list__items')
+    
+    data.forEach(item => {
+      let card =
+        `<article class="appartments-item" tabindex="0">
 
-
-// !!!!!!!!!!!!!!!SCROLL TO ANCHOR!!!!!!!!!!!!!
-
-
-$(function(){
-  $("a[href^='#']").click(function(){
-    var hrefLink = $(this).attr("href");
-    $("html, body").animate({scrollTop: $(hrefLink).offset().top}, 1000);
-    return false;
-  });
-});
-
-
-// !!!!!!!!!!!!!!SLIDER!!!!!!!!!!!!!!!!!!!!
-
-
-$('.cakebox-list').owlCarousel ({
-  dots: true,
-  loop: true,
-  center: true,
-  nav: false,
-  responsive : {
-    0 : {
-        items : 1,
-    },
-    767 : {
-        items : 1,
-        dots: false,
-        nav: true,
-        navText: ["<img src='img/icons/left-icon.svg'>", "<img src='img/icons/right-icon.svg'>"],
-    }
+        </article>`
+      list.appendChild(card)
+    })
   }
-});
 
-$('.sizebox-list').owlCarousel ({
-  loop: true,
-  responsive : {
-      0 : {
-          items : 1,
-      },
-  }
-});
+  $.ajax('https://api.myjson.com/bins/6dbie!!!!!!!!!!')
+    .done(data => {
+    loadData(data)
+  })
+    .fail(function() {
+    alert('error');
+  })
 
-$('.appearbox-list').owlCarousel ({
-  loop: true,
-  margin: 10,
-  responsive : {
-      0 : {
-          items : 1,
-      },
-      767 : {
-          items : 2,
-          dots: false,
-          nav: true,
-          navText: ["<img src='img/icons/left-icon.svg'>", "<img src='img/icons/right-icon.svg'>"],
-      }
-  }
-});
+};
 
-$('.reviews__list').owlCarousel ({
-  loop: true,
-  dots: true,
-  responsive : {
-      0 : {
-          items : 1,
-      },
-      950 : {
-          items : 1,
-          dots: false,
-          nav: true,
-          navText: ["<img src='img/icons/left-icon.svg'>", "<img src='img/icons/right-icon.svg'>"],
-      }
-  }
-});
+function outcomeCount() {
+  const incomeNum = getElementById('details-total__income').textContent.parseInt;
+  const plusNum = getElementById('details-total__plus').textContent.parseInt;
+  const minusNum = getElementById('details-total__minus').textContent.parseInt;
 
-$(window).width(function() {
-  if ($(window).width() > 767) {
-    $('.sizebox-list').trigger('destroy.owl.carousel');
-  }
-  else {
-    return false;
-  }
-});
+  const outcome = incomeNum + plusNum + minusNum;
 
-// !!!!!!!!!!POPUP!!!!!!!!!!!!!
+  document.getElementById('details-total__outcome').innerHTML(outcome);
 
-$('.final-order__btn').on('click', function() {
-  if ($('.choice-cakebox, .choice-sizebox, .choice-appearbox').html() == '') {
-    alert('Выберите все параметры для заказа');
-  }
-  else {
-    $('.popup').fadeIn(500);
-  }
-});
-
-$('.popup-cancel__btn').on('click', function() {
-  $('.popup').fadeOut(500);
-});
-
-// !!!!!!!!!!МАСКА ДЛЯ ВВОАД ТЕЛЕФОНА!!!!!!!!!!!!
-
-$(function($){
-  $("#phone").mask("(999) 999-9999");
-});
+}
 
 
-// !!!!!!!!!!ВЫБОР КОМПОНЕНТОВ ДЛЯ ЗАКАЗА!!!!!!!!!!!!
 
-
-$('.cakebox-block__btn').on('click', function() {
-  $('.choice-cakebox').text($(this).prev('.cakebox-block').children('.cakebox-block__title').text().split('(')[0]);
-});
-
-
-$('.personal-sizebox').on('click', function() {
-  $('.choice-sizebox').text($(this).children('.personal-sizebox__title').text().split('(')[0]);
-});
-
-$('.personal-sizebox__btn').on('click', function() {
-  $('.choice-sizebox').text($(this).prev('.personal-sizebox').children('.personal-sizebox__title').text().split('(')[0]);
-});
-
-
-$('.personal-appearbox').on('click', function() {
-  $('.choice-appearbox').text($(this).children('.personal-appearbox__title').text().split('(')[0]);
-});
-
-$('.personal-appearbox__btn').on('click', function() {
-  $('.choice-appearbox').text($(this).prev('.personal-appearbox').prev('.personal-appearbox').children('.personal-appearbox__title').text().split('(')[0]);
-});
-
-
-// !!!!!!!!!!!!!!!!VISITED LINKS!!!!!!!!!!!!
-
-$('.main-nav__link').on('click', function() {
-  $(this).css({'color':'blue'});
-});
-
-
-// !!!!!!!!!!!!!!ВЫБОР КОМПОНЕНТОВ ПО КЛИКУ!!!!!!!!!!!!!!!!!!!!!!!
-
-$('.personal-sizebox,.personal-appearbox').on('click', function() {
-  $(this).children('i').toggleClass('.active');
-  $(this).children('i').toggle();
-})
-
-});
